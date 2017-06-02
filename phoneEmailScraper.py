9defcebad7ac3f088834aeec27c1198d59156926
CalamitousCamel <CalamitousCamel@users.noreply.github.com>
2017-06-02 12:31:15 -0600

    added final touches to phoneEmailScraper

--- phoneEmailScraper.py
+++ phoneEmailScraper.py
@@ -8,20 +8,19 @@ phoneRegex= re.compile(r'''(?i)										# note the inline ignorecase function
 							\D?										# spacer
 							\(?(?P<a>\d{3})?\)?						# 3 digit area code incl. optional bracket notation
 							\D?								
-							(?P<n1>\d{3})\D?(?P<n2>\d{4})				# 7 digit phone number
-							((\D*(x|extension)\D{,3}(?P<ext>\d+)))?	# extension matching within written sentence matches common extension identifiers with numbers at most 3 spaces afterwards. Matches a flexible distance away from the initial phone number to account for differing scentence structure 
+							(?P<n1>\d{3})\D?(?P<n2>\d{4})			# 7 digit phone number
+							((\D*(x|extension)\D{,3}(?P<ext>\d+)))?	# extension matching. Matches a flexible distance away from the initial phone number to account for differing scentence structure 
 		    			''',re.X)	
 
 emailRegex= re.compile(r'''
-			(\S+\@\S+) #matches any non-whitespace character on either side of the @
+			(\S+\@\S+) 												# matches any non-whitespace character on either side of the @
 			''',re.X)
 
-#copiedText= str(pyperclip.paste())
-copiedText = "if 16138346612 834 6612 then if 834 6612 then if 1 613 834 6612 ext. 244 then if (613)8346612 X 244 then if 834 6612 next then 834 6612  this is a block of text that includes a number 8346612 with the extension 224  more text for the number 1 (613) 834 6612 with the extension 224  or the other number 834 6612. call that, oh ask for x 2244. there is also some email's I want you to have: 234554@registereduser.ca or call him at 1 306 265 4455 ext. 88 or xxlcose_xx@86canada.ec.gc.ca" 
+copiedText= str(pyperclip.paste())
 phoneMatch=[]
 emailMatch=[]
 
-for number in phoneRegex.finditer(copiedText):
+for number in phoneRegex.finditer(copiedText):						# this if-mess translates each various phone number match into it's own string (accounting for differing/missing notation) and appends it to a list of phone numbers
 		phoneNum= str(number.group('n1')+'.'+number.group('n2'))
 		if number.group('a') != None:
 			phoneNum = str(number.group('a')+'.'+phoneNum)
@@ -31,27 +30,16 @@ for number in phoneRegex.finditer(copiedText):
 			phoneNum += str(' ext.'+number.group('ext'))
 		phoneMatch.append(phoneNum)
 
-for email in emailRegex.findall(copiedText):
+for email in emailRegex.findall(copiedText):						# finds and appends emails into a list
 	emailMatch.append(email)
 
-allMatch='\nphone numbers found: '+str(len(phoneMatch))
-'''####################DEBUG#########################	
-print('\nphone numbers found: '+str(len(phoneMatch)))
+allMatch='\nphone numbers found: '+str(len(phoneMatch))+'\n'		# begins each section with a report on how many of each match was found in the text and then pipes a comma delimited string "list" into allMatch
 for n in phoneMatch:
-	print(n)
+	allMatch+=n+', '
 
-print('\nemails matched: '+str(len(emailMatch)))
+allMatch+=('\nemails found: '+str(len(emailMatch))+'\n')
 for n in emailMatch:
-	print(n)
-'''
-
+	allMatch+=n+', '
 
 pyperclip.copy(allMatch)
 
-'''TODO
-imort text from clipboard
-search text for info
-format info into readable text
-export to clipboard
-throw error if none detected (try/except function?)
-'''
