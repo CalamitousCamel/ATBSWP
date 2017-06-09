252d14181ea0102935b180b9e6376d59c84c5bc5
CalamitousCamel <CalamitousCamel@users.noreply.github.com>
2017-06-09 12:33:41 -0600

    rStrip adds default options to re.sub to mimic str.strip()

--- regexStrip.py
+++ regexStrip.py
@@ -1,8 +1,16 @@
 #! python3
-# a version of str.strip() nut with regex. For reasons.
+# a version of str.strip() but with regex.
 import re
 
-toStrip = '   this is a test   '
-char = '(^\s*\b)(?:.*)(\b\s*$)'
-def rStrip(toStrip,char):
-	re.sub(toStrip,char)
+# pattern matches the leading and trailng space in any string
+defaultChar = re.compile(r'(^\s*\b)(.*)(\b\s*$)')
+
+def rStrip(toStrip,char=defaultChar):
+	# if no char arg then strip whitespace by returning matching group 2 only
+	if char is defaultChar:	
+		newString = re.sub(char,r'\2',toStrip)
+	else:
+		newString = re.sub(char,'',toStrip)
+	print(newString)
+
+rStrip('   this is a test   ','[asi]')	
