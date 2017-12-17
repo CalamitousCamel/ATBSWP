288b1c13a2dc739249f34fedaa20d04255739202
CalamitousCamel <CalamitousCamel@users.noreply.github.com>
2017-12-16 18:52:17 -0600

    appeased pylint (for the most part)

--- lucky.py
+++ lucky.py
@@ -1,11 +1,12 @@
-#!Python3
+# !Python3
+# takes command arguments and opens the search resualts of the top 5 links.
 
 import sys
 import requests
 import bs4
 import webbrowser
 import logging
-#logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 logging.basicConfig(level=logging.DEBUG,
                     format=' %(asctime)s - %(levelname)s - %(message)s')
 
@@ -13,15 +14,14 @@ print('Googling...')
 req = requests.get('https://www.google.ca/search?q=' + ' '.join(sys.argv[1:]))
 req.raise_for_status()
 
-#TODO retireve top search resu
-#lt links
+#retireve top search resu#lt links
 souped = bs4.BeautifulSoup(req.text, 'html.parser')
 links = souped.select('.r a')
 logging.debug(len(links))
 logging.debug(links[0].getText())
-assert type(links) is list
+assert isinstance(links, list)
 
-#TODO open a browser tab for each result
-numOpen = min(3, len(links))
+#open a browser tab for each result
+numOpen = min(5, len(links))
 for i in range(numOpen):
     webbrowser.open_new_tab('https://google.com' + links[i].get('href'))
